d0af2a146d94fc05c073ea45f519fa2a9b453fa6
AbdullahDawoud <abdullahdawoud@gmail.com>
2022-08-01 11:11:54 +0300

    ..

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import MyNotesApp from './MyNotesApp';
 import reportWebVitals from './reportWebVitals';
 
